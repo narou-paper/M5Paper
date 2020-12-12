ed8187e66b530545b749e37deae5c8ce35cf1022
ynakano1127 <ynakano1127@gmail.com>
2020-12-12 18:48:22 +0900

    [add]AP立ててWebサーバーを実行

--- src/frame/frame_receive.cpp
+++ src/frame/frame_receive.cpp
@@ -1,6 +1,43 @@
 #include "frame_receive.h"
+#include "WiFi.h"
+#include "WebServer.h"
 
-M5EPD_Canvas receive_canvas(&M5.EPD);
+WebServer _web_server(80);
+M5EPD_Canvas _receive_canvas(&M5.EPD);
+xTaskHandle xHandle;
+
+void webOpenHandler(void *pvParameter){
+    while(1){
+        _web_server.handleClient();
+    }
+}
+
+void webOpen()
+{
+    WiFi.softAP("Narou-Paper-M5Paper","mokemoke");
+    IPAddress myIP = WiFi.softAPIP();
+    Serial.print("AP IP address: ");
+    Serial.println(myIP);
+
+    _web_server.begin();
+    xTaskCreatePinnedToCore(webOpenHandler, "webOpen", 4096, NULL, 1, &xHandle, 0);
+};
+
+void webClose()
+{
+    _web_server.stop();
+    vTaskDelete(xHandle);
+    WiFi.softAPdisconnect(true);
+};
+
+void handleRoot(){
+    _web_server.send(200, "text/plain", "Hello");
+};
+
+void handleNotFound()
+{
+    _web_server.send(404, "text/plain", "Oh No.");
+};
 
 Frame_Receive::Frame_Receive(void)
 {
@@ -12,6 +49,9 @@ Frame_Receive::Frame_Receive(void)
 
     _key_exit->AddArgs(EPDGUI_Button::EVENT_RELEASED, 0, (void*)(&_is_run));
     _key_exit->Bind(EPDGUI_Button::EVENT_RELEASED, &Frame_Base::exit_cb);
+
+    _web_server.on("/", handleRoot);
+    _web_server.onNotFound(handleNotFound);
 };
 
 Frame_Receive::~Frame_Receive(void)
@@ -21,13 +61,13 @@ Frame_Receive::~Frame_Receive(void)
 
 int Frame_Receive::run()
 {
-    receive_canvas.pushCanvas(120, 430, UPDATE_MODE_DU4);
     return 1;
 };
 
 void Frame_Receive::exit()
 {
-    receive_canvas.deleteCanvas();
+    webClose();
+    _receive_canvas.deleteCanvas();
 }
 
 int Frame_Receive::init(epdgui_args_vector_t &args)
@@ -36,16 +76,19 @@ int Frame_Receive::init(epdgui_args_vector_t &args)
 
     M5.EPD.WriteFullGram4bpp(GetWallpaper());
 
-    receive_canvas.createCanvas(300,100);
-    receive_canvas.fillCanvas(15);
-    receive_canvas.setTextSize(26);
-    receive_canvas.setTextColor(0);
-    receive_canvas.setTextDatum(CC_DATUM);
-    receive_canvas.drawString("Hello. Send jpg image!", 150, 55);
+    _receive_canvas.createCanvas(300,100);
+    _receive_canvas.fillCanvas(15);
+    _receive_canvas.setTextSize(26);
+    _receive_canvas.setTextColor(0);
+    _receive_canvas.setTextDatum(CC_DATUM);
+    _receive_canvas.drawString("Hello. Send jpg image!", 150, 55);
+    _receive_canvas.pushCanvas(120, 430, UPDATE_MODE_DU4);
 
     _canvas_title->pushCanvas(0, 8, UPDATE_MODE_NONE);
 
     EPDGUI_AddObject(_key_exit);
+
+    webOpen();
 
     return 3;
 };
